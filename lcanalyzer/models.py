705171139ce99ced3ac9fa082db7759fc1c5b689
ilicdragana <dr.dragana.ilic@googlemail.com>
2024-02-06 11:13:10 +0100

    Docstring improvements.

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -21,13 +21,21 @@ def load_dataset(filename):
 
 
 def mean_mag(data,mag_col):
-    """Calculate the mean magnitude of a lightcurve"""
-    return data[mag_col].mean().
+    """Calculate the mean magnitude of a lightcurve
+    :param data: pd.DataFrame with observed magnitudes for a single source.
+    :param mag_col: a string with the name of the column for calculating the mean.
+    :returns: A float with mean value of the column
+    """
+    return data[mag_col].mean()
 
 
 def max_mag(data,mag_col):
-    """Calculate the max magnitude of a lightcurve"""
-    return data[mag_col].max().
+    """Calculate the max magnitude of a lightcurve
+    :param data: pd.DataFrame with observed magnitudes for a single source.
+    :param mag_col: a string with the name of the column for calculating the max value.
+    :returns: The max value of the column.
+    """
+    return data[mag_col].max()
 
 
 def min_mag(data,mag_col):
@@ -36,4 +44,4 @@ def min_mag(data,mag_col):
     :param mag_col: a string with the name of the column for calculating the min value.
     :returns: The min value of the column.
     """
-    return data[mag_col].min().
+    return data[mag_col].min()
